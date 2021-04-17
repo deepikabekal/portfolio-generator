ee0e05876360a4af0a550f4b8b2bd244fc5759f1
Deepika Bekal <deepika.bekal2@gmail.com>
2021-04-17 14:34:29 +0400

    intial codes

---
+++ app.js
@@ -0,0 +1,30 @@
+// console.log("Hello World");
+// console.log(document);
+
+// var message = "Hello Node!"
+// var sum = 5 + 3;
+// console.log(message);
+// console.log(sum);
+
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+// console.log(process);
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    // This...
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+      console.log(profileDataArr[i]);
+    }
+  
+    console.log('================');
+  
+    // Is the same as this...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+  };
+  
+  printProfileData(profileDataArgs);
+    
