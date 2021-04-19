38de681749b27b94f793feedd3b15713a600d618
Deepika Bekal <deepika.bekal2@gmail.com>
2021-04-19 14:58:52 +0400

    add gitignore file, download and import inquirer package and use prompt method

--- app.js
+++ app.js
@@ -28,24 +28,40 @@
   
 //   printProfileData(profileDataArgs);
 
-const fs = require('fs');
-const generatePage = require("./src/page-template.js"); 
+const inquirer = require("inquirer");
+
+// const fs = require('fs');
+// const generatePage = require("./src/page-template.js"); 
+
+// const pageHTML = generatePage(name, github);
     
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+//const profileDataArgs = process.argv.slice(2, process.argv.length);
 //console.log(profileDataArgs);
 
 // const name = profileDataArgs[0];
 // const github = profileDataArgs[1];
 
-const [name, github] = profileDataArgs;
+//const [name, github] = profileDataArgs;
 
 
 
 // console.log(name, github);
 // console.log(generatePage(name, github));
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
 
-    console.log('Portfolio complete! Check out index.html to see  the output');
-});
+//     console.log('Portfolio complete! Check out index.html to see  the output');
+// });
+
+inquirer
+    .prompt([
+        {
+            type : "input",
+            name : "name",
+            message : "What is your name?"
+
+
+        }
+    ])
+    .then (answers => console.log(answers));
