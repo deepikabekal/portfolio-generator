402157cdbe72c289dcf335ce5d42f853f5bd0156
Deepika Bekal <deepika.bekal2@gmail.com>
2021-04-19 18:52:06 +0400

    validate user answer

--- app.js
+++ app.js
@@ -71,12 +71,34 @@ const inquirer = require("inquirer");
             {
                 type: "input",
                 name: "name",
-                message: 'What is your name?'
+                message: 'What is your name?(Required)',
+                validate : nameInput => {
+                    if (nameInput) 
+                    {
+                        return true;
+                    } 
+                    else 
+                    {
+                        console.log("Please enter your name.");
+                        return false;
+                    }
+                }
             },
             {
                 type : "input",
                 name : "github",
-                message : "Enter your GitHub username?"
+                message : "Enter your GitHub username?(required)",
+                validate : githubUserInput => {
+                    if (githubUserInput)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        console.log("Enter your GitHub username.");
+                        return false;
+                    }
+                }
             },
             {
                type : "input",
@@ -103,12 +125,34 @@ const inquirer = require("inquirer");
             {
                 type : "input",
                 name : "name",
-                message : "What is the name of your project?"
+                message : "What is the name of your project?(required)",
+                validate : projectNameInput =>{
+                    if (projectNameInput)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        console.log("Enter project name.");
+                        return false;
+                    }
+                }
             },
             {
                 type : "input",
                 name : "description",
-                message : "Provide a description of the project (Required)."
+                message : "Provide a description of the project (Required).",
+                validate : projectDescInput => {
+                    if (projectDescInput)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        console.log("Enter project description.");
+                        return false;
+                    }
+                }
             },
             {
                 type : "checkbox",
@@ -119,7 +163,18 @@ const inquirer = require("inquirer");
             {
                 type : "input", 
                 name : "link",
-                message : "Enter the GitHub link to your project. (Required)"
+                message : "Enter the GitHub link to your project. (Required)",
+                validate : gitHubLinkInput => {
+                    if(gitHubLinkInput)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        console.log("Enter GitHub link to your project.");
+                        return false;
+                    }
+                }
             },
             {
                 type : "confirm",
