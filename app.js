1aeb29593ed6ad68692c64c1dbfd02a9230e17ca
Deepika Bekal <deepika.bekal2@gmail.com>
2021-04-20 14:09:05 +0400

    validate answers by using when

--- app.js
+++ app.js
@@ -101,10 +101,27 @@ const inquirer = require("inquirer");
                 }
             },
             {
+                type : "confirm",
+                name : "confirmAbout",
+                message : "Would you like to enter some information about yourself for an 'About' section?",
+                default : true
+            },
+            {
                type : "input",
                name : "about",
-               message : "Provide some information about yourself." 
+               message : "Provide some information about yourself.",
+               when : ({confirmAbout}) => {
+                   if (confirmAbout)
+                   {
+                       return true;
+                   }
+                   else
+                   {
+                       return false;
+                   }
+               }
             }
+            
         ]);
     };
 
